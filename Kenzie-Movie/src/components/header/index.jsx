fd5fec7eac080245c7e650c9ec6d20b9322eb1a7
Hyven <lucasface99@gmail.com>
2023-10-28 01:47:36 -0300

    header adicionado
    cabeçalho previamente iniciado

---
+++ Kenzie-Movie/src/components/header/index.jsx
@@ -0,0 +1,17 @@
+import style from "./style.module.scss"
+
+export const Header = () => {
+    return (
+        <header className={style.container__header}>
+            <nav>
+                <div className={style.container__logo}>kenziemovie</div>
+                <ul>
+                    <li>
+                        <h3>Cadastre-se</h3>
+                        <button className="btn">Entrar</button>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
