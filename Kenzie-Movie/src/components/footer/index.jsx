ced85ce534b4cefbfecc98fe5195e9af05df8a11
Hyven <lucasface99@gmail.com>
2023-10-28 19:44:55 -0300

    correção no footer

--- Kenzie-Movie/src/components/footer/index.jsx
+++ Kenzie-Movie/src/components/footer/index.jsx
@@ -1,5 +1,8 @@
 export const Footer = () => {
-    <>
-        <h3>todos os direitos reservados</h3>
-    </>
+
+    return (
+        <>
+            <h3>todos os direitos reservados</h3>
+        </>
+    )
 }
