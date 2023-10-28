1a67d69fd5ad3a680c35d064bbdfb60c6af1b050
Hyven <lucasface99@gmail.com>
2023-10-28 01:13:30 -0300

    libs instaladas

--- Kenzie-Movie/src/App.jsx
+++ Kenzie-Movie/src/App.jsx
@@ -1,9 +1,22 @@
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
 
   return (
     <>
-
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        closeButton={true}
+      />
     </>
   )
 }
