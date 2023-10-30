4bb4b440d76cba67cbe270acffd47b09ae22f22b
Eduardo <127069576+Pikirito@users.noreply.github.com>
2023-10-30 17:33:52 -0400

    Feat/Style for LoginPage

--- Kenzie-Movie/src/components/forms/FormLogin/index.jsx
+++ Kenzie-Movie/src/components/forms/FormLogin/index.jsx
@@ -3,40 +3,52 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema } from "./loginSchema";
 import { Input } from "../Input";
 import { useContext } from "react";
-import { userContext } from "../../../providers/userContext"
+import { userContext } from "../../../providers/userContext";
+import style from "./index.module.scss";
+import { Link } from "react-router-dom";
 
 export const FormLogin = () => {
+  const { userLogin } = useContext(userContext);
 
-	const { userLogin } = useContext(userContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: zodResolver(loginSchema) });
 
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm({ resolver: zodResolver(loginSchema) });
+  const submit = (formData) => {
+    userLogin(formData, reset);
+  };
 
-	const submit = (formData) => {
-		userLogin(formData, reset)
-	};
-
-	return (
-		<div>
-			<form onSubmit={handleSubmit(submit)}>
-				<Input
-					type="email"
-					placeholder="E-mail"
-					error={errors.email}
-					{...register("email")}
-				/>
-				<Input
-					type="password"
-					placeholder="Senha"
-					error={errors.password}
-					{...register("password")}
-				/>
-				<button type="submit">Entrar</button>
-			</form>
-		</div>
-	);
+  return (
+    <div className={style.mainContent}>
+      <div className={style.loginFormDiv}>
+        <h2 className="title1">Login</h2>
+        <form onSubmit={handleSubmit(submit)}>
+          <Input
+            type="email"
+            placeholder="E-mail"
+            error={errors.email}
+            {...register("email")}
+          />
+          <Input
+            type="password"
+            placeholder="Senha"
+            error={errors.password}
+            {...register("password")}
+          />
+          <div className={style.loginButtons}>
+            <button className="btn" type="submit">
+              Entrar
+            </button>
+            <p className="paragraph">Ou</p>
+            <Link to="/register" className="link">
+              Cadastre-se
+            </Link>{" "}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
