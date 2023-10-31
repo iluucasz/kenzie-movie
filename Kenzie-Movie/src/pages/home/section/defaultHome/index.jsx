8a20a49ae3519216bc0d22c39927fcb1255aa735
Hyven <lucasface99@gmail.com>
2023-10-31 17:12:55 -0300

    parte do userHome estilizado

--- Kenzie-Movie/src/pages/home/section/defaultHome/index.jsx
+++ Kenzie-Movie/src/pages/home/section/defaultHome/index.jsx
@@ -5,38 +5,41 @@ import { MdOutlineStarBorderPurple500 } from "react-icons/md";
 export const DefaultHome = () => {
     return (
         <>
-            <section className={style.container__defaultTemplate}>
-                <img src="https://res.cloudinary.com/dsbkp5841/image/upload/v1688390764/Rectangle_5_mgqd46.jpg" alt="imagem de um filme" />
+            <div className="container">
+                <section className={`${style.container__defaultTemplate}`}>
+                    <img src="https://res.cloudinary.com/dsbkp5841/image/upload/v1688390764/Rectangle_5_mgqd46.jpg" alt="imagem de um filme" />
 
-                <div className={style.container__main}>
-                    <div className={style.container__category}>
-                        <span className={`${style.btn__category} btn medium menu-item`}>ficção</span>
-                        <h3 className={`${style.title__category} title1`}>The random Heros</h3>
-                    </div>
+                    <div className={style.container__main}>
+                        <div className={style.container__category}>
+                            <span className={`${style.btn__category} btn medium menu-item`}>ficção</span>
+                            <p className={`${style.category__time} paragraph`}>90m</p>
+                        </div>
 
-                    <div className={style.container__review}>
-                        <p className={`${style.review__time} paragraph`}>90m</p>
-                        <div className={`${style.container__star}`}>
-                            <button >
-                                <MdOutlineStarBorderPurple500 size={40} className={style.star__icon} />
-                            </button>
-                            <p className="paragraph">5.0</p>
+                        <div className={style.container__review}>
+                            <h3 className={`${style.title__category} title1`}>The random Heros</h3>
+                            <div className={`${style.container__star}`}>
+                                <button >
+                                    <MdOutlineStarBorderPurple500 size={40} className={style.star__icon} />
+                                </button>
+                                <p className="paragraph">5.0</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <section>
-                <div>
-                    <ul className={style.container__cardList}>
-                        <CardList />
-                        <CardList />
-                        <CardList />
-                        <CardList />
-                        <CardList />
-                    </ul>
-                </div>
-            </section>
+                <section>
+                    <div>
+                        <ul className={style.container__cardList}>
+                            <CardList />
+                            <CardList />
+                            <CardList />
+                            <CardList />
+                            <CardList />
+                        </ul>
+                    </div>
+                </section>
+            </div>
+
         </>
     )
 }
