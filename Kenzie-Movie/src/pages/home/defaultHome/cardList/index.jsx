62b09b2ba5b466a013d02d328d4524b2894cfa54
Joel Patrick <joelpatrick15@gmail.com>
2023-11-01 17:13:26 -0300

    Dashboard
    Alimentação do conteúdo da dashboard dinâmico.
    
    Adionada média das notas dos filmes.

--- Kenzie-Movie/src/pages/home/defaultHome/cardList/index.jsx
+++ Kenzie-Movie/src/pages/home/defaultHome/cardList/index.jsx
@@ -1,25 +1,29 @@
 import { MdOutlineStarBorderPurple500 } from "react-icons/md";
 import style from "./style.module.scss";
+import { useReviewContext } from "../../../../providers/reviewContext";
 
-export const CardList = () => {
+export const CardList = ({movie}) => {
+
+    const {calculateReviewScore} = useReviewContext()
+
     return (
         <li className={style.container__card}>
-            <img src="https://res.cloudinary.com/dsbkp5841/image/upload/v1688390764/Rectangle_5_mgqd46.jpg" alt="imagem" />
+            <img src={movie.image} alt="imagem" />
 
             <div className={style.container__main}>
 
                 <div className={style.container__category}>
-                    <span className={`${style.btn__category} btn medium menu-item`}>ficção</span>
-                    <h3 className="title1-mobile-b">the Road</h3>
+                    <span className={`${style.btn__category} btn medium menu-item`}>{movie.type}</span>
+                    <h3 className="title1-mobile-b">{movie.name}</h3>
                 </div>
 
                 <div className={style.container__review}>
-                    <p className={`${style.review__time} paragraph`}>90m</p>
+                    <p className={`${style.review__time} paragraph`}>{movie.duration}m</p>
                     <div className={`${style.container__star}`}>
                         <button >
                             <MdOutlineStarBorderPurple500 size={40} className={style.star__icon} />
                         </button>
-                        <p className="paragraph">5.0</p>
+                        <p className="paragraph">{calculateReviewScore(movie.id)}</p>
                     </div>
                 </div>
 
