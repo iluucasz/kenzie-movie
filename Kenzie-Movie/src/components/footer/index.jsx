bfab2cca590a4ea06424407442e8da82796ddf2d
Joel Patrick <joelpatrick15@gmail.com>
2023-11-01 02:02:48 -0300

    fix LoginPage
    Imagem da loginPage de acordo com o figma.

--- Kenzie-Movie/src/components/footer/index.jsx
+++ Kenzie-Movie/src/components/footer/index.jsx
@@ -2,7 +2,7 @@ import style from "./style.module.scss";
 
 export const Footer = () => {
   return (
-    <footer className={style.footer}>
+    <footer>
       <h3 className="title1-mobile-b">Todos os direitos reservados - Kenzie Academy Brasil</h3>
     </footer>
   );
--- Kenzie-Movie/src/components/forms/FormLogin/index.jsx
+++ Kenzie-Movie/src/components/forms/FormLogin/index.jsx
@@ -8,47 +8,53 @@ import style from "./index.module.scss";
 import { Link } from "react-router-dom";
 
 export const FormLogin = () => {
-  const { userLogin } = useContext(userContext);
+	const { userLogin } = useContext(userContext);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm({ resolver: zodResolver(loginSchema) });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm({ resolver: zodResolver(loginSchema) });
 
-  const submit = (formData) => {
-    userLogin(formData, reset);
-  };
+	const submit = (formData) => {
+		userLogin(formData, reset);
+	};
 
-  return (
-    <div className={`${style.mainContent}`}>
-      <div className={`${style.container__login}`}>
-        <form onSubmit={handleSubmit(submit)} className={`${style.loginFormDiv}`}>
-          <h2 className="title1">Login</h2>
-          <Input
-            type="email"
-            placeholder="E-mail"
-            error={errors.email}
-            {...register("email")}
-          />
-          <Input
-            type="password"
-            placeholder="Senha"
-            error={errors.password}
-            {...register("password")}
-          />
-          <div className={style.loginButtons}>
-            <button className="btn menu-item" type="submit">
-              Entrar
-            </button>
-            <p className="paragraph">Ou</p>
-            <Link to="/register" className="link">
-              Cadastre-se
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`${style.mainContent}`}>
+			<img
+				src="https://s3-alpha-sig.figma.com/img/2476/4c2b/fda1cc43ba3bdb1afb516afbbe432268?Expires=1699833600&Signature=XOvEllK2MFAfw8iH39mPZRRLzZmMowmDbQcojPtAOqA8Pl80wMQDEM49X8wzd9WOvEQJlqIAXA5VxRV0bpVeh8c2FiVlDsnkTH0Z2xKxFu4CRQSkwbfK2DpkgZhwoqn~mqPW-1HaNENQBZVEsSudNskuimlwQMhvm4JLY93o4hIGCE7RxzF0lw919g3EqCyD5~MjIXHnqJDlpqy9unO7bL6ewQtWS-BilEMTTAkXJAwRBj98jlZMKqkCxbxxnlJLHlgfI-jC-pKRRJQI4ci3~djUZXgTRgXpXWBnkE36FedPPXzhKP4rIjWVSy2zydwbGOBbndZfnqxurLOf4faocA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+			/>
+			<div className={`${style.container__login}`}>
+				<form
+					onSubmit={handleSubmit(submit)}
+					className={`${style.loginFormDiv}`}
+				>
+					<h2 className="title1">Login</h2>
+					<Input
+						type="email"
+						placeholder="E-mail"
+						error={errors.email}
+						{...register("email")}
+					/>
+					<Input
+						type="password"
+						placeholder="Senha"
+						error={errors.password}
+						{...register("password")}
+					/>
+					<div className={style.loginButtons}>
+						<button className="btn menu-item" type="submit">
+							Entrar
+						</button>
+						<p className="paragraph">Ou</p>
+						<Link to="/register" className="link">
+							Cadastre-se
+						</Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
