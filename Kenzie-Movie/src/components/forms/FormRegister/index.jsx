aa155c401c4f616fa0db421b66baa2177c230444
Joel Patrick <joelpatrick15@gmail.com>
2023-11-02 17:02:15 -0300

    edit styles

--- Kenzie-Movie/src/components/forms/FormRegister/index.jsx
+++ Kenzie-Movie/src/components/forms/FormRegister/index.jsx
@@ -22,35 +22,35 @@ export const FormRegister = () => {
 
 	return (
 		<form className={style.registerForm} onSubmit={handleSubmit(submit)}>
-				<div>
-					<Input
-						type="text"
-						placeholder="Nome"
-						error={errors.name}
-						{...register("name")}
-					/>
-					<Input
-						type="email"
-						placeholder="E-mail"
-						error={errors.email}
-						{...register("email")}
-					/>{" "}
-				</div>
+			<div>
+				<Input
+					type="text"
+					placeholder="Nome"
+					error={errors.name}
+					{...register("name")}
+				/>
+				<Input
+					type="email"
+					placeholder="E-mail"
+					error={errors.email}
+					{...register("email")}
+				/>
+			</div>
 
-				<div>
-					<Input
-						type="password"
-						placeholder="Senha"
-						error={errors.password}
-						{...register("password")}
-					/>
-					<Input
-						type="password"
-						placeholder="Confirmar Senha"
-						error={errors.confirmPassword}
-						{...register("confirmPassword")}
-					/>
-				</div>
+			<div>
+				<Input
+					type="password"
+					placeholder="Senha"
+					error={errors.password}
+					{...register("password")}
+				/>
+				<Input
+					type="password"
+					placeholder="Confirmar Senha"
+					error={errors.confirmPassword}
+					{...register("confirmPassword")}
+				/>
+			</div>
 
 			<button className="btn menu-item" type="submit">
 				Cadastrar-se
